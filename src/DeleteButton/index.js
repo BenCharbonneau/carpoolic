6723cb006b00256262b33d93f93911462b2bda8f
agraham <agraham0925@gmail.com>
2018-05-15 11:51:51 -0500

    created ag-branch from master and added delete db route to react side

--- src/DeleteButton/index.js
+++ src/DeleteButton/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const delRide = async (reState,e) => {
 	const id = e.currentTarget.id
-	await fetch('database rides/delete/'+id,{
+	await fetch('http://localhost:9292/rides/' + id,{
 		credentials: 'include'
 	});
 
