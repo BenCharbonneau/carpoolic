9c529bdd900a6e811c13b72fd289c2e2b8c50dc7
BenCharbonneau <benscharb@gmail.com>
2018-05-15 16:24:15 -0500

    Created Modal and updated Banner

--- src/App.js
+++ src/App.js
@@ -16,28 +16,12 @@ class App extends Component {
     console.log(id);
     this.setState({loggedIn: true, userId: id});
   }
-
-getRides = async () => {
-  const ridesJSON = await fetch('http://localhost:9292/rides', {
-    credentials: 'include'
-    // body: JSON.stringify({
-    //   username: username,
-    //   password: password
-    // })
-  });
- 
-  const rides = await ridesJSON.json();
-  const available_rides = rides.retrieved_rides
-  console.log(available_rides)
-  return available_rides;
-}
   render() {
     return (
       <div className="App">
         {this.state.loggedIn ?
           <div>
             <HomePageContainer userId={this.state.userId} />
-            <RideSearchContainer getRides={this.getRides} />
           </div>:
           <Login setLoggedIn={this.setLoggedIn}/>    
         }
--- src/RideSearchContainer/index.js
+++ src/RideSearchContainer/index.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
+import ShowRide from '../ShowRide';
+import Modal from '../Modal';
 
-const RideSearch = (props) => {
-  
-  const rideList = props.rides.map((ride) => {
+class UserRideList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      rides: [],
+      modalClass: 'closed',
+      ride: -1
+    }
+  }
+  componentDidMount() {
+    this.getRides().catch((err) => {
+      console.log(err);
+    })
+  }
+  getRides = async () => {
+    try {
+      const ridesJSON = await fetch('http://localhost:9292/rides', {
+        credentials: 'include'
+        // body: JSON.stringify({
+        //   username: username,
+        //   password: password
+        // })
+      });
+
+      const rides = await ridesJSON.json();
+
+      this.setState({rides: rides.retrieved_rides});
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+  rideShow = (e) => {
+    this.setState({modalClass: 'open', ride: e.currentTarget.id})
+  }
+  rideHide = () => {
+    this.setState({modalClass: 'closed', ride: -1})
+  }
+  render() {
+
+    const rides = this.state.rides.map((ride) => {
+      return (
+        <li key={ride.id} id={ride.id} onClick={this.rideShow}>
+          <ShowRide userId={this.props.userId} fields={['name','pickup','destination','pickup_time','driver','delete']} rideId={ride.id} close={this.getRides}/> 
+        </li>
+      );
+    })
+
+    const showComp = <ShowRide userId={this.props.userId} rideId={this.state.ride} close={this.rideHide} />
+
     return (
-      <li key={ride.id}>
-        ride description: {ride.name} <br />
-        ride destination: {ride.destination} <br />
-        available seats: {ride.passenger_slots} <br />
-        pickup date: {ride.pickup_date} <br />
-        pickup time: {ride.pickup_time} <br />
-        pickup location: {ride.pickup} <br />
-      </li>
+
+      <div>
+        <ul>{rides}</ul>
+        <Modal comp={showComp} cssClass={this.state.modalClass}/>
+      </div>
     );
-  })
-  
-  return (
-    <div>
-      <ul>{rideList}</ul>
-    </div>
-  );
+  }
 }
 
-export default RideSearch;
+export default UserRideList;
